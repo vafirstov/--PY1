33769b19cd3b243043b41f7ccaedbebac2cafdfa
vafirstov <115399589+vafirstov@users.noreply.github.com>
2022-10-21 20:33:27 +0300

    Update task1.py

--- Лабораторная работа №3/task1.py
+++ Лабораторная работа №3/task1.py
@@ -1,7 +1,9 @@
 src = not False and True or False and not True
 
 # TODO расписать упрощение выражения
-
+# not False = True, следовательно первая часть выражения True and True = True
+# not True = False, следовательно вторая часть выражения False and False = False
+# Итог: True or False = True
 result = True
 
 print(src == result)
